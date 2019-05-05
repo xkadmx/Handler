f78453b1e1af594c4d3845ec99f812c54c9f7fa2
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-05 14:58:50 +0200

    initializing Handler in main thread

--- app/src/main/java/com/example/handler/MainActivity.java
+++ app/src/main/java/com/example/handler/MainActivity.java
@@ -1,24 +1,38 @@
 package com.example.handler;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView myText;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myText = (TextView) findViewById(R.id.textView);
+        myText = (TextView) findViewById(R.id.myText);
+        handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+            }
+        }
 
         new Thread(new Runnable() {
             @Override
             public void run() {
 
+
             }
-        })
+        }).start();
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.VIEW");
+        sendBroadcast(intent);
     }
 }
